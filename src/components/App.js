dd4e0f88d9904f8ee3efda043a58d4c8641326eb
GeoffNixCodes <97037675+GeoffNixCodes@users.noreply.github.com>
2022-10-27 18:12:34 -0400

    put axios in own component

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import axios from 'axios';
 import SearchBar from './SearchBar';
-
+import Unsplash from "../api/Unsplash";
+import ImageList from "./ImageList";
 
 
 class App extends React.Component {
-   async onSearchSubmit(term) {
-    const response = await axios
-        .get('https://api.unsplash.com/search/photos', {
-        params: {query: term},
-        headers: {
-            Authorization: 'Client-ID O3lXMJZeu3UraqqAIOSmmmpULfUHqmbh2fGWvg-H9qQ'
-        }
-    });
-    console.log(response.data.results);
+    state = { images: [] };
+
+    onSearchSubmit = async (term) => {
+        const response = await Unsplash
+            .get('/search/photos', {
+                params: {query: term},
+            });
+        this.setState({images: response.data.results});
     }
 
     render() {
         return (
             <div className="ui container" style={{marginTop: '10px'}}>
                 <SearchBar onSubmit={this.onSearchSubmit}/>
+               <ImageList images={this.state.images} />
             </div>
         );
     };
